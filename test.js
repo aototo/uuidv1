8c31ea866f6090c9435c7fd6c76eaa5a1d108189
Jackens <jacek.czekaj@gmail.com>
2018-10-15 13:04:16 +0200

    test.js: uuidv1.js and uuidv1.min.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const uuidv1 = require('./uuidv1.min.js')
+const uuidv1 = require('./uuidv1.js')
+const uuidv1Min = require('./uuidv1.min.js')
 
 for (let i = 0; i < 16 * 16 * 16 + 1; i++) {
-  console.log(uuidv1())
+  console.log(uuidv1(), uuidv1Min())
 }
